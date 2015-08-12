da868a4ce1896a2fa9e4caf5d4c7895551a2ae4d
umbrella <308048801@qq.com>
2015-08-12 18:11:57 +0800

    mark
    mark

---
+++ main.js
@@ -0,0 +1,11 @@
+//entry
+
+define(function(require, exports, module){
+        
+    /*   modules from brakcets */
+    
+    var CommandManager = require("CommandManager"),
+        Menu           = require("Menu");
+        
+    
+});
