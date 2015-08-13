d4ed4561954c40c6eac877f12013d81673ee3dcf
umbrella <308048801@qq.com>
2015-08-13 16:36:18 +0800

    main setting
    main setting

--- main.js
+++ main.js
@@ -6,17 +6,38 @@ define(function(require, exports, module){
     
     var CommandManager  = brackets.getModule("command/CommandManager"),
         DocumentManager = brackets.getModule("document/DocumentManager"),
-        Menus           = brackets.getModule("command/Menus");
+        Menus           = brackets.getModule("command/Menus"),
+        EditorManager   = brackets.getModule("editor/EditorManager");
         
     
     var MENU_Name = "YourNoteTool",
         COMMAND_ID   = "note.tool";
     
-    var keys = JSON.parse(require("text!shortcut.json"));
+    var keys = JSON.parse(require("text!shortcut.json")),
+        _html = require("text!modal/index.html");
     
+    var _template = Mustache.render(_html, null);
+    
+   
     function OnMenuClickHandler(){
         //insert 
         //
+        //alert('OnMenuClickHandler');
+        var _curDoc = DocumentManager.getCurrentDocument();
+        if(!_curDoc)
+            return;
+    
+        var _fuledit = EditorManager.getCurrentFullEditor();
+        
+        if(!_fuledit)
+            return;
+        
+        
+         var from = {line: 0, ch: 0};
+        var to = {line: 10, ch: 10};
+        EditorManager.getCurrentFullEditor().setSelection(from, to, true);
+        
+        
     }
     
     
