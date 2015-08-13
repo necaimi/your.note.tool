42b06441d3664b6c8a035dc8cf2ba8f55d1ab3ce
umbrella <308048801@qq.com>
2015-08-13 10:48:26 +0800

    main.js
    modify require() to brackets.getModule

--- main.js
+++ main.js
@@ -4,8 +4,25 @@ define(function(require, exports, module){
         
     /*   modules from brakcets */
     
-    var CommandManager = require("CommandManager"),
-        Menu           = require("Menu");
+    var CommandManager  = brackets.getModule("CommandManager"),
+        DocumentManager = brackets.getModule("document/DocumentManager"),
+        Menus           = brackets.getModule("Menus");
         
     
+    var MENU_Name = "YourNoteTool",
+        COMMAND_ID   = "note.tool";
+    
+    
+    function OnMenuClickHandler(){
+        //insert 
+        //
+    }
+    
+    
+    CommandManger.register(MENU_Name, COMMAND_ID, OnMenuClickHandler);
+    var menu = Menus.getMenu(Menus.AppMenuBar.NAVIGATE_MENU);
+        menu.addMenuDivider();
+        menu.addMenuItem(_ID, "ctrl-alt-i");
+        
+        
 });
