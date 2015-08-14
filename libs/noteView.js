73f1354620d32de63924db948fb6ddeaee9329dc
umbrella <308048801@qq.com>
2015-08-14 15:47:31 +0800

    slow...
    slow...

--- libs/noteView.js
+++ libs/noteView.js
@@ -4,34 +4,25 @@ define(function(require, exports, module){
    
     "use strict";
     
-    var CommandManager = brackets.getModule("comman/CommandManager"),
-        InlineWidget   = brackets.getModule("edit/InlineWidget").InlineWidget,
+    var InlineWidget   = brackets.getModule("editor/InlineWidget").InlineWidget,
         ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
     
     var items = {};
     
-    function noteView(){
+    ExtensionUtils.loadStyleSheet(module, "webform.less");
+    
+    function noteView(pros){
         InlineWidget.call(this);
+        var _template = Mustcache.render(template, pros);
+        
+        this.$htmlContent.append(_template);
+        
     }
     
     
     noteView.prototype = Object.create(InlineWidget.prototype);
-    noteView.prototype.addItem = function(title, cmd){
-        
-        var _id = cmd.getID();
-        if(items[_id])
-            return;
-        
-        var _item = $("<li/>");
-        _item.bind("click", function(e){
-            CommandManager.execute(_id);
-        });
-     
-        items[_id] = _item;
-        
-    }
+    noteView.prototype.constructor = InlineWidget;
     
-    
-    
+    module.exports = noteView;
     
 });
--- main.js
+++ main.js
@@ -7,37 +7,31 @@ define(function(require, exports, module){
     var CommandManager  = brackets.getModule("command/CommandManager"),
         DocumentManager = brackets.getModule("document/DocumentManager"),
         Menus           = brackets.getModule("command/Menus"),
-        EditorManager   = brackets.getModule("editor/EditorManager");
+        NoteView        = require("./libs/noteView").noteView;
         
     
     var MENU_Name = "YourNoteTool",
         COMMAND_ID   = "note.tool";
     
-    var keys = JSON.parse(require("text!shortcut.json")),
-        _html = require("text!modal/index.html");
+    var keys = JSON.parse(require("text!./shortcut.json")),
+        _html = require("text!./modal/index.html");
     
-    var _template = Mustache.render(_html, null);
-    
-   
+    var _plugins = JSON.parse(require("text!./plugins.json"));
+        if(_plugins){
+            var _plugins = _plugins.plugins;
+            for(var itr = 0, _len = _plugins.length; itr < _len; itr++ ){
+                var _t = _plugins[itr];
+                require(_t.path);
+            }
+        }
+        
+
     function OnMenuClickHandler(){
-        //insert 
-        //
-        //alert('OnMenuClickHandler');
         var _curDoc = DocumentManager.getCurrentDocument();
         if(!_curDoc)
             return;
-    
-        var _fuledit = EditorManager.getCurrentFullEditor();
         
-        if(!_fuledit)
-            return;
-        
-        
-         var from = {line: 0, ch: 0};
-        var to = {line: 10, ch: 10};
-        EditorManager.getCurrentFullEditor().setSelection(from, to, true);
-        
-        
+        var _nV = new noteView(_plugins);
     }
     
     
